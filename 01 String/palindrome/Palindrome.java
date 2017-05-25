75be706ae5dc6ce598e323d84afde92ea4dab373
Shruti Saha <shruti.saha21@gmail.com>
2017-05-25 22:18:55 +0530

    Improve function palindrome
    Using less library fuctions, variables, redeced LOC.

--- 01 String/palindrome/Palindrome.java
+++ 01 String/palindrome/Palindrome.java
@@ -1,25 +1,27 @@
 public class Palindrome {
 	
-	static void isPalindrome(String str1)
+	//O(n/2)
+	static boolean isPalindrome(String str1)
 	{
 		Boolean isPalindrom=true;
-		//String str1 = System.console().readLine();
-		int ln = str1.length();
-		int l2 = (int) Math.ceil(ln); 
-		for(int i=0; i<l2; i++)
+		int startIndex = str1.length()-1;
+		int endIndex = str1.length()-1; 
+		while(startIndex>endIndex)
 		{
-			if(str1.charAt(i)!=str1.charAt(ln-1-i))
+			if(str1.charAt(startIndex)!=str1.charAt(endIndex))
 				isPalindrom = false;
+			startIndex++;
+			endIndex--;
 		}
-		if(isPalindrom)
-			System.out.println(str1 +" is palindrom");
-		else
-			System.out.println(str1 + " is not polindrom");
+		return isPalindrom;
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		isPalindrome(args[0]);
+		
+		boolean isPalindrom = isPalindrome(args[0]);
+		if(isPalindrom)
+			System.out.println(args[0] +" is palindrom");
+		else
+			System.out.println(args[0] + " is not polindrom");
 	}
-
 }
