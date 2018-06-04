1311883a848629132475bdb1fdfaa3fba97b2ec6
Adi <adithecomputerguy@gmail.com>
2018-06-04 21:26:59 +0530

    Going through pupular string problems.
    -Find the number of words in a camelCaseWords.

---
+++ 01 String/CamelCase/CamelCaseWords.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace substring
+{
+    /// <summary>
+    /// Find the number of words in a camelCaseWords
+    /// e.g. camelCaseWords has 3 words
+    /// </summary>
+    class CamelCaseWords
+    {
+        internal int FindNumberOfWords(string words)
+        {
+            int count = 1;
+            foreach (char ch in words)
+            {
+                if ((int)ch > 65 && (int)ch < 90)
+                    ++count;
+            }
+            return count;
+        }
+    }
+}
