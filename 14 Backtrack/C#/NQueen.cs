57e0aaf24eea97eb1bbc0c3cf9c384ebcc3e6ef4
AdiTCG <adithecomputerguy@gmail.com>
2018-09-24 20:24:51 +0530

    Backtrack approach to solve NQueen problem.

---
+++ 14 Backtrack/C#/NQueen.cs
@@ -0,0 +1,13 @@
+﻿namespace Backtrack
+{
+    public class NQueen
+    {
+        public int[] GetQueenPositions(int n)
+        {
+            int[] positions = new int[n];
+
+
+            return positions;
+        }
+    }
+}
--- 99 Misc/_VisualStudio/Preparation/MainDriverClass.cs
+++ 99 Misc/_VisualStudio/Preparation/MainDriverClass.cs
@@ -9,20 +9,19 @@ namespace Preparation
         static void Main(string[] args)
         {
             Fibonacci f = new Fibonacci();
-
             var sw = Stopwatch.StartNew();
-            //var series = f.GetFibonacci(44);
-            //sw.Stop();
-            //Console.Write(series + ", ");
-            //Console.WriteLine("==========Elapsed time=========");
-            //Console.WriteLine(sw.Elapsed);
-
             sw = Stopwatch.StartNew();
             var series1 = f.GetFibonacciWithDPWithoutDictionary(50);
             sw.Stop();
             Console.Write(series1 + ", ");
             Console.WriteLine("==========Elapsed time=========");
             Console.WriteLine(sw.Elapsed);
+
+            //var series = f.GetFibonacci(44);
+            //sw.Stop();
+            //Console.Write(series + ", ");
+            //Console.WriteLine("==========Elapsed time=========");
+            //Console.WriteLine(sw.Elapsed);
             //BinarySearchTree<int> myTree = new BinarySearchTree<int>();
             //myTree.Insert(5);
             //myTree.Insert(3);
