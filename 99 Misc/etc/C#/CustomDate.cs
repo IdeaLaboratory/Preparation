7fdce97c6d2930e278627e3c400b87ff08dde94b
AdiTCG <adithecomputerguy@gmail.com>
2018-12-28 16:10:48 +0530

    Date validation:  - Custom dates and it's validation method.

---
+++ 99 Misc/etc/C#/CustomDate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Etc
+{
+    class CustomDate
+    {
+        public int DD { get; set; }
+        public int MM { get; set; }
+        public int YYYY { get; set; }
+
+        public CustomDate(int d, int m, int y)
+        {
+            DD = d; MM = m; YYYY = y;
+        }
+
+        public bool IsValid()
+        {
+            return false;
+        }
+    }
+}
