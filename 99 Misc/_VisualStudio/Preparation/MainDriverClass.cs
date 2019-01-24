3fa62396dc1825909d6232e31ab460dc61112a0e
AdiTCG <adithecomputerguy@gmail.com>
2019-01-24 22:25:08 +0530

    Added random number generator with biasedness.

--- 99 Misc/_VisualStudio/Preparation/MainDriverClass.cs
+++ 99 Misc/_VisualStudio/Preparation/MainDriverClass.cs
@@ -1,8 +1,7 @@
-﻿using DP;
+﻿using Etc;
 using System;
-using System.Diagnostics;
-using Strings;
-using Etc;
+using System.IO;
+using System.Text;
 
 namespace Preparation
 {
@@ -10,84 +9,99 @@ namespace Preparation
     {
         static void Main(string[] args)
         {
-            // find max power
-            MaxPower mxPow = new MaxPower();
-            Console.WriteLine(mxPow.FindMaxPower(75));
-            Console.WriteLine(mxPow.FindMaxPower(617));
-
-            //CustomDate check
-
-            CustomDate date = new CustomDate(29, 2, 2016);
-            Console.WriteLine(date.IsValid());
-            CustomDate date1 = new CustomDate(29, 2, 2017);
-            Console.WriteLine(date1.IsValid());
-            CustomDate date2 = new CustomDate(29, 21, 2016);
-            Console.WriteLine(date2.IsValid());
-            CustomDate date3 = new CustomDate(29, 2, 3016);
-            Console.WriteLine(date3.IsValid());
-            CustomDate date4 = new CustomDate(33, 2, 2016);
-            Console.WriteLine(date4.IsValid());
-
-            GroupOccurrencesCharacters obj = new GroupOccurrencesCharacters();
-            var newString = obj.GroupOccChar("abcac");
-            Console.WriteLine(newString);
-
-
-            Tree.Trie trie = new Tree.Trie();
-            trie.Insert("hel");
-            trie.Insert("helaate");
-            trie.Insert("help");
-            trie.Insert("hell");
-            trie.Insert("hello");
-            trie.Insert("helps");
-            trie.Insert("has");
-            trie.Insert("hes");
-
-            var list = trie.Suggestions("hel");
-
-            foreach (var item in list)
+            for (int j = 0; j < 100; j++)
             {
-                Console.WriteLine(item);
+                var csv = new StringBuilder();
+                Probability p = new Probability();
 
+                for (int i = 0; i < 9000; i++)
+                {
+                    Console.WriteLine();
+                    //Suggestion made by KyleMit
+                    csv.AppendLine(p.GetRandomNumberInRange(0, 99, 80, 99, 0.04).ToString());
+                }
+                File.WriteAllText(@"C:\del\t.csv", csv.ToString());
             }
+            //after your loop
 
-            Console.WriteLine();
-            Console.WriteLine();
+            //// find max power
+            //MaxPower mxPow = new MaxPower();
+            //Console.WriteLine(mxPow.FindMaxPower(75));
+            //Console.WriteLine(mxPow.FindMaxPower(617));
 
-            Fibonacci f = new Fibonacci();
-            var sw = Stopwatch.StartNew();
-            sw = Stopwatch.StartNew();
-            ////////////Write your code here to measure performance////////////
-            //Backtrack.NQueen nQueen = new Backtrack.NQueen();
-            //int n = 16;
-            //int[,] positions = new int[n, n];
-            //nQueen.GetQueenPositions(positions, 0);
+            ////CustomDate check
 
-            //for (int i = 0; i < positions.GetLength(0); i++)
+            //CustomDate date = new CustomDate(29, 2, 2016);
+            //Console.WriteLine(date.IsValid());
+            //CustomDate date1 = new CustomDate(29, 2, 2017);
+            //Console.WriteLine(date1.IsValid());
+            //CustomDate date2 = new CustomDate(29, 21, 2016);
+            //Console.WriteLine(date2.IsValid());
+            //CustomDate date3 = new CustomDate(29, 2, 3016);
+            //Console.WriteLine(date3.IsValid());
+            //CustomDate date4 = new CustomDate(33, 2, 2016);
+            //Console.WriteLine(date4.IsValid());
+
+            //GroupOccurrencesCharacters obj = new GroupOccurrencesCharacters();
+            //var newString = obj.GroupOccChar("abcac");
+            //Console.WriteLine(newString);
+
+
+            //Tree.Trie trie = new Tree.Trie();
+            //trie.Insert("hel");
+            //trie.Insert("helaate");
+            //trie.Insert("help");
+            //trie.Insert("hell");
+            //trie.Insert("hello");
+            //trie.Insert("helps");
+            //trie.Insert("has");
+            //trie.Insert("hes");
+
+            //var list = trie.Suggestions("hel");
+
+            //foreach (var item in list)
             //{
-            //    for (int j = 0; j < positions.GetLength(0); j++)
-            //    {
-            //        Console.Write(positions[i, j] + ",");
-            //    }
-            //    Console.WriteLine();
+            //    Console.WriteLine(item);
+
             //}
 
-            String s1 = "AGCAT";
-            String s2 = "GACXXA";
+            //Console.WriteLine();
+            //Console.WriteLine();
 
-            char[] X = s1.ToCharArray();
-            char[] Y = s2.ToCharArray();
-            int m = X.Length;
-            int n = Y.Length;
+            //Fibonacci f = new Fibonacci();
+            //var sw = Stopwatch.StartNew();
+            //sw = Stopwatch.StartNew();
+            //////////////Write your code here to measure performance////////////
+            ////Backtrack.NQueen nQueen = new Backtrack.NQueen();
+            ////int n = 16;
+            ////int[,] positions = new int[n, n];
+            ////nQueen.GetQueenPositions(positions, 0);
 
-            Console.Write("Length of LCS is" + " "
-                          + GFG.lcs(X, Y, m, n));
+            ////for (int i = 0; i < positions.GetLength(0); i++)
+            ////{
+            ////    for (int j = 0; j < positions.GetLength(0); j++)
+            ////    {
+            ////        Console.Write(positions[i, j] + ",");
+            ////    }
+            ////    Console.WriteLine();
+            ////}
 
-            ////////////////////////
-            sw.Stop();
-            Console.WriteLine("==========Elapsed time=========");
-            Console.WriteLine(sw.Elapsed);
-            Console.Read();
+            //String s1 = "AGCAT";
+            //String s2 = "GACXXA";
+
+            //char[] X = s1.ToCharArray();
+            //char[] Y = s2.ToCharArray();
+            //int m = X.Length;
+            //int n = Y.Length;
+
+            //Console.Write("Length of LCS is" + " "
+            //              + GFG.lcs(X, Y, m, n));
+
+            //////////////////////////
+            //sw.Stop();
+            //Console.WriteLine("==========Elapsed time=========");
+            //Console.WriteLine(sw.Elapsed);
+            //Console.Read();
 
             //var series = f.GetFibonacci(44);
             //sw.Stop();
---
+++ 99 Misc/etc/C#/Probability.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Etc
+{
+    public class Probability
+    {
+        public int GetRandomNumberInRange(int min, int max, int maskMin, int maskMax, double expProb)
+        {
+            Random r = new Random();
+            int rNum = r.Next(min, max); //for ints
+
+            if (rNum >= 80 && rNum <= 99)
+            {
+                rNum = ProbabilityMaskUtil(rNum, maskMin, maskMax, expProb * 100);
+            }
+
+            return rNum;
+        }
+
+        private int ProbabilityMaskUtil(int val, int min, int max, double expProbability)
+        {
+            HashSet<int> bucket = new HashSet<int>();
+            Random r = new Random();
+
+            FillBucketUtil(min, max, expProbability, bucket, r);
+
+            if (bucket.Contains(val))
+            {
+                return val;
+            }
+            else
+            {
+                return r.Next(0, min - 1);
+            }
+        }
+
+        private static void FillBucketUtil(int min, int max, double expProbability, HashSet<int> bucket, Random r)
+        {
+            int rNum = r.Next(min, max); //for ints
+
+            for (int i = 0; i < expProbability; i++)
+            {
+                if (bucket.Contains(rNum))
+                {
+                    FillBucketUtil(min, max, expProbability - i, bucket, r);
+                    return;
+                }
+                bucket.Add(rNum);
+            }
+
+            rNum = r.Next(min, max); //for ints
+        }
+    }
+}
