780872f9069a39c5ae34119046bee21cc9f00b45
AdiTCG <adithecomputerguy@gmail.com>
2019-02-03 11:35:46 +0530

    Added Arithmetic class and GetHighestIncreasingNumber().

---
+++ 99 Misc/etc/C#/Arithmetic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Etc
+{
+    public class Arithmetic
+    {
+        /// <summary>
+        /// provides the highest increasing number.
+        /// Example: if 121 is given as the limit, HighestIncreasing number will be 119;</summary>
+        /// <returns></returns>
+        public int GetHighestIncreasingNumber(int limit)
+        {
+            int result = 0;
+            return result;
+        }
+    }
+}
